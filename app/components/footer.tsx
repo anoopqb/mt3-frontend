e6f628de3b3919c061d9cb310dd322cf77033466
Anoop John <anoopjohn@qburst.com>
2025-11-15 18:31:19 +0530

    Add footer component, update global styles, and include new dependencies for Strapi and Husky

---
+++ app/components/footer.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BlocksRenderer, type BlocksContent } from "@strapi/blocks-react-renderer";
+import "@/app/components/footer.scss";
+
+interface FooterProps {
+    logoUrl: string;
+    logoAlt: string;
+    addressLane1: string;
+    addressLane2: string;
+    zipCode: string;
+    phoneNumber: string;
+    officeHoursTitle: string;
+    officeHours: any[];
+    copyright: string;
+}
+
+
+
+export default function Footer({
+    logoUrl,
+    logoAlt,
+    addressLane1,
+    addressLane2,
+    zipCode,
+    phoneNumber,
+    officeHoursTitle,
+    officeHours,
+    copyright = `© ${new Date().getFullYear()} All rights reserved.`,
+}: FooterProps) {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-left">
+                    <div className="footer-logo">
+                        <Link href="/">
+                            <Image src={logoUrl} alt={logoAlt} width={129} height={43} />
+                        </Link>
+                    </div>
+                    <div className="footer-contact">
+                        <div className="footer-address">
+                            <p>{addressLane1}</p>
+                            <p>
+                                {addressLane2}
+                                {zipCode}
+                            </p>
+                        </div>
+                        <div className="footer-phone">
+                            <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-right">
+                    <h3 className="footer-hours-title">{officeHoursTitle}</h3>
+                    <div className="footer-hours">
+                        <BlocksRenderer content={officeHours} />
+                    </div>
+                </div>
+            </div>
+            <div className="footer-copyright">
+                <p>{copyright}</p>
+            </div>
+        </footer>
+    );
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/header";
 import { fetchApiData } from "@/lib/strapi";
+import Footer from "./components/footer";
 
 export const metadata: Metadata = {
   title: "MT3 Website",
@@ -24,12 +25,27 @@ interface HeaderApiResponse {
   }
 }
 
+interface FooterApiResponse {
+  data: {
+    logoUrl: string;
+    AddressLane1: string;
+    AddressLane2: string;
+    ZipCode: string;
+    PhoneNumber: string;
+    OfficeHoursTitle: string;
+    OfficeHours: any[];
+    copyright: string;
+  }
+}
 const globalData = await fetchApiData<GlobalApiResponse>("global?pLevel=3");
 const headerData = await fetchApiData<HeaderApiResponse>("header?pLevel=3");
+const footerData = await fetchApiData<FooterApiResponse>("footer?pLevel=3");
 const global = globalData.data;
 const header = headerData.data;
+const footer = footerData.data;
 
 
+console.log(footer.OfficeHours);
 
 export default function RootLayout({
   children,
@@ -44,6 +60,18 @@ export default function RootLayout({
       >
         <Header logoUrl={`${process.env.NEXT_PUBLIC_URL}${global.logo.url}`} logoAlt="Logo" logoLocation={header.logoLocation} navItems={header.NavMenu} />
         {children}
+
+        <Footer
+          logoUrl={`${process.env.NEXT_PUBLIC_URL}${global.logo.url}`}
+          logoAlt="Logo"
+          addressLane1={footer.AddressLane1}
+          addressLane2={footer.AddressLane2}
+          zipCode={footer.ZipCode}
+          phoneNumber={footer.PhoneNumber}
+          officeHoursTitle={footer.OfficeHoursTitle}
+          officeHours={footer.OfficeHours}
+          copyright={footer.copyright}
+        />
       </body>
     </html >
   );
